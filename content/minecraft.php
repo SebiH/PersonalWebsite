58d33e0937426a716119f374053a470ed6de8b9a
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2014-09-20 21:37:48 +0200

    Added extremely basic site for minecraft downloads

--- content/minecraft.php
+++ content/minecraft.php
@@ -1,7 +1,9 @@
-TODO
-
-<?php
-
-echo "Hello world";
-
-?>
+<center> 
+    <p> TODO </p>
+    <p> 
+        <a href="../data/SebisSoundMix0.3.zip"> SoundMix </a> 
+    </p>  
+    <p> 
+        <a href="../data/SebisTextureMix1.8v1.zip"> TexturePack </a> 
+    </p>  
+</center>
