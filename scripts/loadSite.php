6edffbc1c8d182b39919188a39c114f1198b2133
root <root@sehub.de>
2014-10-26 14:08:19 +0100

    Minor fixes for previous commit

--- scripts/loadSite.php
+++ scripts/loadSite.php
@@ -1,32 +1,32 @@
 <?php
 $aResult = array();
+$filename = $_POST['sitename'];
 
-if( !isset($_POST['sitename']) ) { $aResult['error'] = '<center><p>No function name!</p></center>'; }
+// load home if no site specified
+if (empty($filename))
+    $filename = "home";
 
-if( !isset($aResult['error']) ) {
+// prevent searching outside of the content folder
+$filename = str_replace(".", "", $filename);
+$filename = str_replace("/", "", $filename);
+$filename = str_replace("~", "", $filename);
 
-    // prevent searching outside of the content folder
-    $filename = str_replace(".", "", $_POST['sitename']);
-    $filename = str_replace("/", "", $filename);
-    $filename = str_replace("~", "", $filename);
-    
-    $filepath = "../content/" . $filename;
+$filepath = "../content/" . $filename;
 
-    if (file_exists($filepath . ".html"))
-        $filepath .= ".html";
-    else if (file_exists($filepath . ".php"))
-        $filepath .= ".php";
-    else
-    {
-        $aResult['error'] = "<center><p>No such file</p><center>";
-        echo json_encode($aResult);
-        return;
-    }
-
-    $contentfile = fopen($filepath, "r");
-    $aResult['result'] = fread($contentfile, filesize($filepath));
-    fclose($contenfile);
+if (file_exists($filepath . ".html"))
+    $filepath .= ".html";
+else if (file_exists($filepath . ".php"))
+    $filepath .= ".php";
+else
+{
+    $aResult['error'] = "<center><p>No such file</p><center>";
+    echo json_encode($aResult);
+    return;
 }
+
+$contentfile = fopen($filepath, "r");
+$aResult['result'] = fread($contentfile, filesize($filepath));
+fclose($contenfile);
 
 
 echo json_encode($aResult);
