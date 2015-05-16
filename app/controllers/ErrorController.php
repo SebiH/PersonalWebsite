c052dd66d1e00e4c4796ddcb7bfbbe233f940796
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2015-05-16 13:18:50 +0200

    Returning proper 404 and 500 status code

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -15,11 +15,13 @@ class ErrorController extends Controller
 
     public function show404Action()
     {
+        $this->response->setStatusCode(404, "Not Found");
     }
 
 
     public function show500Action()
     {
+        $this->response->setStatusCode(500, "Internal Server Error");
     }
 }
 
