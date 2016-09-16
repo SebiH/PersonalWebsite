c6e3164dcd3577ed7eabf7ff6137015a239a442b
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-09-16 19:56:16 +0200

    Fix spf json generator not working

--- homepage/_plugins/spfjson.rb
+++ homepage/_plugins/spfjson.rb
@@ -1,102 +1,83 @@
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Use of this source code is governed by The MIT License.
-# See the LICENSE file for details.
-
-# Jekyll plugin to generate a SPF JSON version of each page.
-#
-# Author:: nicksay@google.com (Alex Nicksay)
-
-
 require 'json'
 
 
 module Jekyll
 
+    class Page
+        def subfolder
+            @dir
+        end
 
-  class SpfJsonPage < Page
-
-    def initialize(site, base, original)
-      @site = site
-      @base = base
-      # Get the @dir value from the original page, not the dir() output.
-      @dir = original.instance_variable_get(:@dir)
-      @name = original.name
-
-      process(@name)
-
-      @content = original.content
-      @data = original.data
+        def html
+            @output
+        end
     end
 
-    def render(layouts, site_payload)
-      super
-      html = @output
+    class SpfJsonPageGenerator < Generator
 
-      title_regex = /<title>(.*)<\/title>/m
-      head_regex = /<!-- begin spf head -->(.*)<!-- end spf head -->/m
-      body_regex = /<!-- begin spf body: (\w+) -->(.*)<!-- end spf body: \1 -->/m
-      foot_regex = /<!-- begin spf foot -->(.*)<!-- end spf foot -->/m
-      attr_body_class_regex = /<body[^>]* class="([^"]*)"/
+        safe true
+        priority :low
 
-      title = html.match(title_regex)[1]
-      head = html.match(head_regex)[1]
-      body = {}
-      html.scan(body_regex).each do |group|
-        body[group[0]] = group[1]
-      end
-      foot = html.match(foot_regex)[1]
-      attr_body_class = html.match(attr_body_class_regex)[1]
-      attrs = {
-        'body' => {'class' => attr_body_class}
-      }
+        def generate(site)
 
-      response = {
-        'title' => title,
-        'head' => head,
-        'body' => body,
-        'attr' => attrs,
-        'foot' => foot,
-      }
-      # Use JSON.pretty_generate instead of response.to_json or JSON.generate
-      # to reduce diff sizes during updates, since the files are checked in.
-      @output = JSON.pretty_generate(response)
+            Jekyll::Hooks.register :pages, :post_write do |page|
+
+                path = page.subfolder + '/' + page.name
+
+                if page.permalink
+                    path = page.permalink
+                end
+
+                path = path[0..-6] if path =~ /.html$/
+                path = File.join(path, 'index') if path =~ /\/$/
+                path = path + '.spf.json'
+
+                dir = site.config['destination']
+
+                # Write the contents
+                File.open(File.join(dir, path), 'w') do |f|
+                    f.write(generate_content(page))
+                    f.close
+                end
+            end
+
+            #site.pages.concat(pages)
+        end
+
+        def generate_content(page)
+            html = page.html
+
+            title_regex = /<title>(.*)<\/title>/m
+            head_regex = /<!-- begin spf head -->(.*)<!-- end spf head -->/m
+            body_regex = /<!-- begin spf body: (\w+) -->(.*)<!-- end spf body: \1 -->/m
+            foot_regex = /<!-- begin spf foot -->(.*)<!-- end spf foot -->/m
+            #attr_body_class_regex = /<body[^>]* class="([^"]*)"/
+
+            title = html.match(title_regex)[1]
+            head = html.match(head_regex)[1]
+            body = {}
+            html.scan(body_regex).each do |group|
+                body[group[0]] = group[1]
+            end
+            foot = html.match(foot_regex)[1]
+            #attr_body_class = html.match(attr_body_class_regex)[1]
+            #attrs = {
+                #'body' => {'class' => attr_body_class}
+            #}
+
+            response = {
+                'title' => title,
+                'head' => head,
+                'body' => body,
+                #'attr' => attrs,
+                'foot' => foot,
+            }
+
+            response
+        end
+
     end
-
-    # Output a .json file.
-    def output_ext
-      '.spf.json'
-    end
-
-    # Masquerade as an .html file.
-    def html?
-      true
-    end
-
-    # Output index.spf.json files, not index.html files.
-    def destination(dest)
-      path = site.in_dest_dir(dest, URL.unescape_path(url))
-      path = File.join(path, 'index.spf.json') if url =~ /\/$/
-      path
-    end
-
-  end
-
-
-  class SpfJsonPageGenerator < Generator
-
-    safe true
-    priority :low
-
-    def generate(site)
-      pages = []
-      site.pages.each do |page|
-        pages << SpfJsonPage.new(site, site.source, page)
-      end
-      site.pages.concat(pages)
-    end
-
-  end
 
 
 end
+
