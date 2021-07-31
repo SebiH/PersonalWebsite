59c3df91d93168d54241f1399c1ebff702f574db
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2021-07-31 11:25:30 +0000

    Minor fixes

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" jekyll/jekyll jekyll build
+docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" jekyll/jekyll:4.2.0 jekyll build
